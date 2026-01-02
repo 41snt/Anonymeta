292e7fbfb06d51ec77e3c4b46a28ffb9da720adf
Gio <giosef.d.yatar@gmail.com>
2026-01-02 23:59:16 +0800

    Refactor typeWrite function for improved readability and consistency

--- main.js
+++ main.js
@@ -3,10 +3,20 @@ const introWords = ["Welcome to anonymeta", "Leave No Trace"];
 
 function typeWrite(words, typeSpeed) {
     for (let i = 0; i < words.length; i++) {
+        let currentWord = words[i];
+
         for (let j = 0; j <= words[i].length; j++) {
-            
+            setTimeout(() => {
+                heroH1.innerHTML = currentWord.substring(0, j);
+            }, typeSpeed);
         }
+
+        typeSpeed += 2000;
+        
+        setTimeout(() => {
+            heroH1.innerHTML = '';
+        }, typeSpeed - 500);
     }
 }
 
-typeWrite(introWords, 100);
+typeWrite(introWords, 2000);
